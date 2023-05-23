a661845e2db278f5d9aaca903961a1d43abe4a1a
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-05-23 10:51:13 +0300

    +

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
 import { GlobalStyle } from '../GlobalStyle';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import Home from 'pages/Home';
+import { useAuth } from '../hooks/useAuth';
 
 export const App = () => {
+  const dispatch = useDispatch();
+
+
   return (
     <div>
+      return isRefreshing ? (<b>Refreshing user...</b>) : (
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="*" element={<Home />} />
+        </Route>
+      </Routes>
       <GlobalStyle />
+      );
     </div>
   );
-}
+};
